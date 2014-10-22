12b0387baa536863e2e84a4d0d4eda26304eb21f
黄柳 <1310421753@qq.com>
2014-10-22 09:44:53 +0800

    Create 2.Tarjan强连通分量.cpp

---
+++ Graph/2.Tarjan强连通分量.cpp
@@ -0,0 +1,31 @@
+int scc,dfn[2*N],low[2*N],curr;  
+#include<stack>  
+stack<int> s;  
+int ins[N];  
+void dfs(int u)  
+{  
+    dfn[u]=low[u]=++curr;  
+    s.push(u);  
+    ins[u]=1;  
+    for(int i=0;i<g[u].size();i++)  
+    {  
+        int v=g[u][i];  
+        if(!dfn[v])  
+        {  
+            dfs(v);  
+            low[u]=min(low[u],low[v]);  
+        }else{  
+            if(ins[v])  
+                low[u]=min(low[u],dfn[v]);  
+        }  
+    }  
+    if(low[u]==dfn[u])  
+    {  
+        ++scc;  int v;  
+        do {  
+            v=s.top(); s.pop();  
+            ins[v]=0;  
+            fa[v]=scc;  
+        }while(v!=u);  
+    }  
+}  
