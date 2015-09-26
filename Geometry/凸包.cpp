67e37363c416fbcbb257fded5a17adbf924d9ec8
黄柳 <1310421753@qq.com>
2015-09-26 17:20:45 +0800

    Update 凸包.cpp

--- Geometry/凸包.cpp
+++ Geometry/凸包.cpp
@@ -1,27 +1,53 @@
 // 点集凸包
 // 如果不希望在凸包的边上有输入点,把两个 <= 改成 <
 // 注意:输入点集会被修改
-vector<Point> CovexHull(vector<Point>& p)
+struct P
 {
-  sort(p.begin(),p.end());
-  p.erase(unique(p.begin(),p.end()),p.end());
-  int n=p.size();
-  int m=0;
-  vector<Point> ch(n+1);
-  for(int i=0;i<n;i++)
+    double x, y;
+    P() {}
+    P(double xx, double yy) { x=xx, y=yy; }
+    P operator - (P b) { return P(x-b.x, y-b.y); }
+    bool operator < (P b) const
     {
-      while(m>1&&Cross(ch[m-1]-ch[m-2],p[i]-ch[m-2])<=0) m--;
-      ch[m++]=p[i];
+        return x < b.x || (x == b.x && y < b.y);
     }
-  int k=m;
-  for(int i=n-2;i>=0;i--)
+    bool operator == (P b) const
     {
-      while(m>k&&Cross(ch[m-1]-ch[m-2],p[i]-ch[m-2])<=0) m--;
-      ch[m++]=p[i];
+        return x==b.x && y==b.y;
     }
-  if(n>1) m--;
-  ch.resize(m);
-  return ch;
+} p[233];
+double dis(P a, P b)
+{
+    return sqrt((a.x-b.x)*(a.x-b.x) + (a.y-b.y)*(a.y-b.y));
+}
+int dcmp(double x) {return x<1e-8 ? -1 : x>1e-8;}
+bool cmp(P a, P b)
+{
+    return a.x < b.x || (a.x == b.x && a.y < b.y);
+}
+double Cross(P a, P b) { return a.x * b.y - a.y *b.x; }
+
+vector<P> ConvexHull(vector<P>& p)
+{
+    sort(p.begin(),p.end());
+    p.erase(unique(p.begin(),p.end()),p.end());
+    int n=p.size();
+    int m=0;
+    vector<P> ch(n+1);
+    for(int i=0; i<n; i++)
+    {
+        while(m>1&&Cross(ch[m-1]-ch[m-2],p[i]-ch[m-2])<0) m--;
+        ch[m++]=p[i];
+    }
+    int k=m;
+    for(int i=n-2; i>=0; i--)
+    {
+        while(m>k&&Cross(ch[m-1]-ch[m-2],p[i]-ch[m-2])<0) m--;
+        ch[m++]=p[i];
+    }
+    if(n>1) m--;
+    ch.resize(m);
+    return ch;
 }
 bool In(vector<P> p, P b)  /// 是否在凸包边界或者内部
 {
