f5e1c09a4585b151a303eff8f5f43b91c03266b2
黄柳 <1310421753@qq.com>
2015-10-14 13:52:45 +0800

    Create 扩栈.cpp

---
+++ Other/扩栈.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+extern int main2(void) __asm__ ("main2");
+
+int main2() {
+    char test[255 << 20];
+    memset(test, 42, sizeof(test));
+    printf(":)\n");
+    exit(0);
+}
+
+int main() {
+    int size = 256 << 20;  // 256Mb
+    char *p = (char *)malloc(size) + size;
+    __asm__ __volatile__(
+        "movq  %0, %%rsp\n"
+        "pushq $exit\n" 
+        "jmp main2\n"
+        :: "r"(p));
+}
